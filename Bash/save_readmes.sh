529a85ef7a18545dafd315ef91d7289e07826289
Sarah Djebali Quelen <sarah.djebali-quelen@toulouse.inra.fr>
2018-02-05 11:11:59 +0100

    Adding bash script to save all README.sh files from one dir to another

--- Bash/save_readmes.sh
+++ Bash/save_readmes.sh
@@ -1,22 +1,50 @@
 
-# create the dirs where a file README.sh is on /work/project/fragencode
-dir=/work/project/fragencode
-find $dir -name README.sh | while read f 
+# usage
+# save_readme.sh sourcedir savedir
+
+# example
+# time ~/fragencode/tools/multi/Scripts/Bash/save_readmes.sh ~/fragencode/workspace/sdjebali ~/save/READMEs_frag
+# real	0m35.087s
+# time ~/fragencode/tools/multi/Scripts/Bash/save_readmes.sh ~/dynagen/sdjebali ~/save/READMEs_dyna
+# real	0m3.145s
+
+# Check the inputs do exist
+###########################
+if [ ! -n "$1" ] || [ ! -n "$2" ]
+then
+    echo "" >&2
+    echo Usage: save_readme.sh sourcedir targetdir >&2
+    echo "" >&2
+    echo "takes as input:" >&2
+    echo "- a source directory under which all the README.sh files will be saved" >&2
+    echo "- a target directory under which all the README.sh found in sourcedir will be copied (saved)" >&2
+    echo "and copies all the README.sh files that are in the tree structure under sourcedir in a comparable" >&2
+    echo "tree structure under targetdir" >&2
+    exit 1
+fi
+
+# Variable assignment
+#####################
+source=$1
+target=$2
+
+# create the dirs where a file README.sh is on $source
+find $source -name README.sh | while read f 
 do 
-file=${f#$dir"/"}
+file=${f#$source"/"}
 echo $file | awk '{s=""; n=split($1,a,"/"); for(k=1; k<=(n-1); k++){s=(s)(a[k])("/"); print s} }' 
 done | while read d
 do
-mkdir -p ~/save/READMEs/$d
+mkdir -p $target/$d
 done
 
 # copy the readmes in the proper places
-find $dir -name README.sh | while read f
+find $source -name README.sh | while read f
 do 
-file=${f#$dir"/"}
-cd ~/save/READMEs/${file%/README.sh}
-cp $dir/$file .
+file=${f#$source"/"}
+cd $target/${file%/README.sh}
+cp $source/$file .
 done
 
 # put the correct rights back
-chmod 755 -R ~/save/READMEs
+chmod 755 -R $target
