b27a21ad5a4deb45463b02f83d2598535cba00da
Sarah Djebali <sarahqd@gmail.com>
2025-10-10 16:11:41 +0200

    removed hard coded path to exec

--- gtfToBed12.sh
+++ gtfToBed12.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+# !!! need kent utils in your paths !!!
+
 set -Eexo pipefail
 
 if [ $# != 1 ]; then
@@ -6,7 +9,7 @@ if [ $# != 1 ]; then
 	echo
 	echo "USAGE: $0 file.gtf"
 	echo
-	echo "From a complete gtf file, generates a GenePred and a bed12 file."
+	echo "Needs kent utils in your path"
 	echo
 	exit 1
 fi
@@ -17,9 +20,9 @@ gtf=$1
 # To create a BED12 from a GTF
 # 1) convert gtf to GenePred (If an entire gtf is given it will report all the genes as errors as it
 # considers only transcripts)
-~sdjebali/save/Kentutils/gtfToGenePred $gtf -allErrors $(dirname $gtf)/$(basename $gtf .gtf).GenePred 2> gtfToGenePred.stderr
+gtfToGenePred $gtf -allErrors $(dirname $gtf)/$(basename $gtf .gtf).GenePred 2> gtfToGenePred.stderr
 # 2) convert GenePred to Bed
-cat $(dirname $gtf)/$(basename $gtf .gtf).GenePred | ~sdjebali/save/Kentutils/genePredToBed12 > $(dirname $gtf)/$(basename $gtf .gtf).bed12
+cat $(dirname $gtf)/$(basename $gtf .gtf).GenePred | genePredToBed12 > $(dirname $gtf)/$(basename $gtf .gtf).bed12
 
 rm gtfToGenePred.stderr
 
