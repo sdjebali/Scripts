d4a10302cbe0408a1dbff880ec2562c5894ae8a0
Sarah Djebali <sarahqd@gmail.com>
2026-01-18 15:56:26 +0100

    adding script to test intersection by hypergeometric test

--- launch_hypergeometric.sh
+++ launch_hypergeometric.sh
@@ -10,25 +10,29 @@ set -Eexo pipefail
 # - 299 in the intersection
 # - 10672 in the universe
 # then we would have to ask the following to know whether it is more than expected by chance
-# phyper(299,1829,10672-1829,1630,lower.tail=FALSE)
-# [1] 0.07593254
+# phyper(299-1,1829,10672+299-1829,1630,lower.tail=FALSE)
+# [1] 0.02789313  *** exactly equivalent to exact fisher test one-sided
 
 # usage:
 # launch_hypergeometric.sh nA nB nA&&B nuniv
+# exactly equivalent to fisher exact test one-sided (see explanations from Andrea in stat.tests.txt)
 
 # example
+# srun --x11 --mem=8G --pty bash
+# cd ~/fragencode/tools/multi/Renv
+# module load statistics/R/4.3.1
 # launch_hypergeometric.sh 1829 1630 299 10672 
 # + '[' '!' -n 1829 ']'
 # + '[' '!' -n 1630 ']'
 # + '[' '!' -n 299 ']'
 # + '[' '!' -n 10672 ']'
 # + R --vanilla --slave
-# + echo 'phyper(299,1829,10672-1829,1630,lower.tail=FALSE)'
-# [1] 0.07593254
+# + echo 'phyper(299-1,1829,10672+299-1829,1630,lower.tail=FALSE)'
+# [1] 0.02789313
 
 if [ ! -n "$1" ] || [ ! -n "$2" ] || [ ! -n "$3" ] || [ ! -n "$4" ]
 then
 echo "usage: launch_hypergeometric.sh nA nB nA&&B nuniv"
 else
-echo 'phyper('$3','$1','$4'-'$1','$2',lower.tail=FALSE)' | R --vanilla --slave
+echo 'phyper('$3'-1,'$1','$4'+'$3'-'$1','$2',lower.tail=FALSE)' | R --vanilla --slave
 fi
