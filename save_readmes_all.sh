86f4380277e1d069ddefd3f8501480e58a4cd5ef
Sarah Djebali <sarahqd@gmail.com>
2026-01-31 18:41:07 +0100

    adding bridge

--- save_readmes_all.sh
+++ save_readmes_all.sh
@@ -1,13 +1,15 @@
 /work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/user/sdjebali /work/project/fragencode/doc/READMEs_sarah/READMEs_work
 /work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/fragencode/workspace/sdjebali /work/project/fragencode/doc/READMEs_sarah/READMEs_frag/workspace/sdjebali
 /work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/fragencode/workspace/geneswitch /work/project/fragencode/doc/READMEs_sarah/READMEs_fraggs
-/work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/geneswitch /work/project/fragencode/doc/READMEs_sarah/READMEs_gs
 /work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/fragencode/papers /work/project/fragencode/doc/READMEs_sarah/READMEs_frag/papers
 /work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/fragencode/results /work/project/fragencode/doc/READMEs_sarah/READMEs_frag/results
 /work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/fragencode/tools /work/project/fragencode/doc/READMEs_sarah/READMEs_frag/tools
-/work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/regenet /work/project/fragencode/doc/READMEs_sarah/READMEs_regencard
 /work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/fragencode/data /work/project/fragencode/doc/READMEs_sarah/READMEs_frag/data
+/work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/bridge/workspace/sdjebali /work/project/fragencode/doc/READMEs_sarah/READMEs_bridge
+
 /work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/dynagen/sdjebali /work/project/fragencode/doc/READMEs_sarah/READMEs_dyna
+/work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/regenet /work/project/fragencode/doc/READMEs_sarah/READMEs_regencard
 /work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/fragencode/workspace/unipluri /work/project/fragencode/doc/READMEs_sarah/READMEs_uni
 /work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/crct/chimeras /work/project/fragencode/doc/READMEs_sarah/READMEs_crct
 /work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/1000rnaseqchick /work/project/fragencode/doc/READMEs_sarah/READMEs_1000rnaseqchick
+/work/project/fragencode/tools/multi/Scripts/save_readmes.sh /work/project/geneswitch /work/project/fragencode/doc/READMEs_sarah/READMEs_gs
